9c8861778fc01726d2114b4cbb6e5046ae07cb7a
chickencoder <jesse_sibley@outlook.com>
2015-06-27 11:40:36 +0100

    Got rid of uglify.. eughh

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,11 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var ugli = require('gulp-uglifycss');
 
 gulp.task('sass', function() {
   gulp.src('./src/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('css', function() {
-  gulp.src('./dist/*.css')
-  .pipe(ugli({}))
-  .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('sass:watch', function() {
