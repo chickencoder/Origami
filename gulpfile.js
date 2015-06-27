e21ce4f66bb6b23a535f54419dd5287c607a48ef
chickencoder <jesse_sibley@outlook.com>
2015-06-27 11:31:06 +0100

    Adjusted gulp and npm files

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,17 @@
+'use strict';
+
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var ugli = require('gulp-uglifycss');
+
+gulp.task('sass', function() {
+  gulp.src('./src/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./src/*.scss', ['sass']);
+});
+
+gulp.default(['sass']);
