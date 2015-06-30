0182c7bf670fb0bd9c84fc355115db8463ba8aa7
chickencoder <jesse_sibley@outlook.com>
2015-06-30 17:37:56 +0100

    Added minify task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,28 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rnme = require('gulp-rename');
+var mcss = require('gulp-minify-css');
 
 gulp.task('sass', function() {
-  gulp.src('./src/*.scss')
+  gulp.src('src/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('sass:watch', function() {
-  gulp.watch('./src/*.scss', ['sass']);
+gulp.task('css', function() {
+  gulp.src('dist/origami.css')
+    .pipe(mcss({compatibility: 'ie8'}))
+    .pipe(rnme({'suffix': '.min'}))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['sass:watch']);
+gulp.task('sass:watch', function() {
+  gulp.watch('src/*.scss', ['sass']);
+});
+
+gulp.task('css:watch', function() {
+  gulp.watch('dist/origami.css', ['css']);
+});
+
+gulp.task('default', ['sass:watch', 'css:watch']);
